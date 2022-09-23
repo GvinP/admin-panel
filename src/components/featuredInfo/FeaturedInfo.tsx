0f2f19eeae76273fd5ab45913c96fd4a0f78e098
alexey kudriashov <gvinpin72@gmail.com>
2022-09-23 17:31:45 +0400

    add revenue data

--- src/components/featuredInfo/FeaturedInfo.tsx
+++ src/components/featuredInfo/FeaturedInfo.tsx
@@ -1,17 +1,41 @@
+import { useState, useEffect } from "react";
 import "./featuredInfo.css";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import { userRequest } from "../../api/config";
+
+type Income = {
+  _id: number;
+  total: number;
+};
 
 export const FeaturedInfo = () => {
+  const [income, setIncome] = useState<Income[]>([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      const res = await userRequest.get<Income[]>("orders/income");
+      console.log(res.data);
+      setPerc((res.data[1]?.total * 100) / res.data[0]?.total - 100);
+      setIncome(res.data);
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Revenue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,875</span>
+          <span className="featuredMoney">$ {income[1]?.total}</span>
           <span className="featuredMoneyRate">
-            -13.7
-            <ArrowDownward className="featuredIcon negative" />
+            % {perc.toFixed(1)}{" "}
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSubtitle">Compared to last month</span>
